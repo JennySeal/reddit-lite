c1d60f841a020070d94d7faab8d99987a90323ec
Jenny Seal <jennysealft@gmail.com>
2021-05-10 10:01:49 +0100

    start to set up store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/Header';
 import Search from './components/Search';
 import Story from './components/Story';
 import './styles/styles.scss'
 
+
+const API_ENDPOINT = 'https://www.reddit.com/search.json?q='
+
+
+
+
+
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [url, setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
+
   return (
     <div className='container'>
     <Header/>
-    <Search/>
+    <Search searchTerm={searchTerm} />
     <Story/>
     </div>
   )
