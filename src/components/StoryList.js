489e003b197d0c1f3a21e286c7cec06c0d9837d8
Jenny Seal <jennysealft@gmail.com>
2021-05-19 11:34:52 +0100

    more sizing issues

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -15,13 +15,13 @@ console.log(stories);
             {stories.data.data.children.map((story) => 
             <div key={story.data.id} className="story">
             <p><img src={story.data.thumbnail} alt="" onError={(e)=>{e.target.onerror = null; e.target.src=story.data.url }}/></p>
+            <StoryPage id={story.data.id} onOpenPost={onOpenPost}/>    
             <div className="storyText">
             <h3>{story.data.title}</h3>
             <p>SubReddit: {story.data.subreddit}</p>
             <p>Author: {story.data.author}</p>
             <p>Ups: {story.data.ups}</p> 
             {story.data.link_flair_text && <p><span className="tag">{story.data.link_flair_text}</span></p>}
-            <StoryPage id={story.data.id} onOpenPost={onOpenPost}/>    
             </div>
         </div>)}
         </div>
