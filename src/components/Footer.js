fdbf7135d80a2396792d9438ca0760510efce934
Jenny Seal <jennysealft@gmail.com>
2021-05-19 10:47:24 +0100

    sizing issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <p>&copy; 2021 - A Codecademy Challenge using the Reddit API - for more projects see: <a href="https://www.jennyseal.com">JennySeal.com</a></p>
+            <p>Swan icon made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
         </footer>
     )
 }
